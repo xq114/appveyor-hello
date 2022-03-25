288e0c70709417291f99f34f4cb1f959060490de
xq114 <1140735506@qq.com>
2022-03-25 16:09:37 +0800

    test llvm

---
+++ get_llvm_ps1.py
@@ -0,0 +1,23 @@
+import sys
+
+PS1_SOURCE = """
+New-Item -Path . -Name llvm-install -ItemType "directory"
+$InstallPath = (Resolve-Path -Path .\llvm-install).Path
+
+Invoke-WebRequest -Uri "https://github.com/llvm/llvm-project/releases/download/llvmorg-{llvmver}/LLVM-{llvmver}-win{bits}.exe" -OutFile .\llvm.exe
+.\llvm.exe /S /D=$InstallPath | Wait-Job
+
+Compress-Archive -Path $InstallPath -DestinationPath .\clang+llvm-{llvmver}-win{arch}.zip
+"""
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Usage: python " + __file__ + " <version> <bits>")
+        exit(1)
+    llvmver = sys.argv[1]
+    bits = "64"
+    if len(sys.argv) > 2:
+        bits = sys.argv[2]
+
+    with open(f"get_llvm_{llvmver}_{bits}.ps1", "w") as f:
+        f.write(PS1_SOURCE.format(llvmver=llvmver, bits=bits))
