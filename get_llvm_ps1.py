6e1d6a5ba700448f597db73815b7fdb386382fe6
xq114 <1140735506@qq.com>
2022-03-25 16:11:32 +0800

    fix

--- get_llvm_ps1.py
+++ get_llvm_ps1.py
@@ -7,7 +7,7 @@ $InstallPath = (Resolve-Path -Path .\llvm-install).Path
 Invoke-WebRequest -Uri "https://github.com/llvm/llvm-project/releases/download/llvmorg-{llvmver}/LLVM-{llvmver}-win{bits}.exe" -OutFile .\llvm.exe
 .\llvm.exe /S /D=$InstallPath | Wait-Job
 
-Compress-Archive -Path $InstallPath -DestinationPath .\clang+llvm-{llvmver}-win{arch}.zip
+Compress-Archive -Path $InstallPath -DestinationPath .\clang+llvm-{llvmver}-win{bits}.zip
 """
 
 if __name__ == '__main__':
